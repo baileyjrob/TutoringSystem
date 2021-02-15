7672440cd9acac15a97b632ff850b722f7cafaf4
Bailey Robinson <baileyjrob@tamu.edu>
2021-02-15 13:21:24 -0600

    Fixed deparment validation

--- app/models/course.rb
+++ app/models/course.rb
@@ -3,5 +3,5 @@ class Course < ApplicationRecord
     belongs_to :departments
     has_and_belongs_to_many :tutoring_sessions
     validates :course_name, presence: true
-    validates :subject_id, presence: true
+    validates :department_id, presence: true
 end
