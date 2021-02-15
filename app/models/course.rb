bd86dcc79de4e90a1d3efefd2ad65635d9e66b79
Bailey Robinson <baileyjrob@tamu.edu>
2021-02-15 13:19:48 -0600

    Renamed Subjects to Departments in models

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,6 +1,6 @@
 class Course < ApplicationRecord
     has_and_belongs_to_many :users
-    belongs_to :subject
+    belongs_to :departments
     has_and_belongs_to_many :tutoring_sessions
     validates :course_name, presence: true
     validates :subject_id, presence: true
