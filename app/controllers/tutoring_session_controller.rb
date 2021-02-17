da4c0ae87d09c6473112a27a4c3c192ea184e9ac
Nick <nick.isom.211@gmail.com>
2021-02-16 23:05:35 -0600

    Comments

--- app/controllers/tutoring_session_controller.rb
+++ app/controllers/tutoring_session_controller.rb
@@ -3,8 +3,7 @@ class TutoringSessionController < ApplicationController
     if cookies.key?('start_week')
       week_offset = 0
       start_week = Time.at(cookies["start_week"].to_f / 1000)
-
-      p "*********************KEY IS " + cookies.key?('week_offset').to_s
+      
       if cookies.key?('week_offset')
         week_offset = cookies['week_offset'].to_f * 1.week
         cookies.delete 'week_offset'
@@ -13,7 +12,7 @@ class TutoringSessionController < ApplicationController
         cookies["start_week"] = (start_week).to_datetime.strftime('%Q')
       end
 
-      start_week += 1.day
+      start_week += 1.day # Have to offset by a day due to how the time parses out when converted from cookie
     else
       start_week = Date.today.beginning_of_week(start_day = :sunday)
       cookies["start_week"] = start_week.to_datetime.strftime('%Q')
@@ -21,9 +20,12 @@ class TutoringSessionController < ApplicationController
 
 
     @week = Hash.new
+
+    # Get all sessions in the week (Might be not needed due to how rails parses queries)
     @tsessions = TutoringSession.
                   where('scheduled_datetime BETWEEN ? AND ?', start_week, start_week + 1.week)
 
+    # Get the sessions on every day and put them into a hash for frontend
     for i in 0..6 
       @week[i] = @tsessions.
                   where('scheduled_datetime BETWEEN ? AND ?', start_week + (i).day, start_week + (i+1).day).
@@ -54,8 +56,9 @@ class TutoringSessionController < ApplicationController
   end
 
   def create
+    # Creates the new session, then adds the tutor to the session
     @tsession = TutoringSession.new(tsession_params)
-    @admin = User.where(first_name: 'Admin')
+    @admin = User.where(first_name: 'Admin') # TODO: Add tutor instead of admin
 
     @tsession.session_status = "new"
     @tsession.users << @admin
@@ -73,7 +76,7 @@ class TutoringSessionController < ApplicationController
 
   def destroy
     @tsession = TutoringSession.find(params[:id])
-    @tsession.delete
+    @tsession.delete # Destroy tries to delete all objects attatched to the session as well. Not good
    
     redirect_to tutoring_session_index_path, notice: "Tutoring session deleted."
   end
