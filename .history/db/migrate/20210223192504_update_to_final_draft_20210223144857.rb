23ca59662e5589723285f0f6def0d82755acd986
Bailey Robinson <baileyjrob@tamu.edu>
2021-02-23 15:53:56 -0600

    Renamed subjects to departments, added foreign-key constraints, added missing attributes. Pre-migration

---
+++ .history/db/migrate/20210223192504_update_to_final_draft_20210223144857.rb
@@ -0,0 +1,16 @@
+class UpdateToFinalDraft < ActiveRecord::Migration[6.1]
+  def change
+    change_table :courses do |t|
+      t.rename :subject_id, :department_id
+    end
+    create_table :departments do |t|
+      t.rename :subject_name, :department_name
+    end
+    create_table :courses_users do |t|
+      t.string :grade_achieved
+    end
+  end
+
+    subject_ID -> department_ID
+  end
+end
