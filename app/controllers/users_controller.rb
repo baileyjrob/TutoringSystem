41c309137f26c51d03a32f4220b60028d028ab96
FrozenLion <28263454+FrozenLion@users.noreply.github.com>
2021-03-19 11:47:56 -0500

    reindented file to make merging easier

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,66 +1,66 @@
 class UsersController < ApplicationController
-  def index
-    @users = User.all
-  end
-
-  def show
-    @user = User.find(params[:id])
-    @tutoring_sessions = TutoringSession.all
-  end
-
-  def new
-    @user = User.new
-  end
-
-  def create
-    @user = User.new(user_params)
-
-    if @user.save
-      redirect_to @user
-    else
-      render :new
+    def index
+        @users = User.all
     end
-  end
 
-  def edit
-    @user = User.find(params[:id])
-  end
-
-  def update
-    @user = User.find(params[:id])
-
-    if @user.update(user_params)
-      redirect_to @user
-    else
-      render :edit
+    def show
+        @user = User.find(params[:id])
+        @tutoring_sessions = TutoringSession.all
     end
-  end
 
-  def destroy
-    @user = User.find(params[:id])
-    @user.destroy
-    redirect_to root_path
-  end
+    def new
+        @user = User.new
+    end
 
-  def schedule_student
-    @user = User.find(params[:id])
-    @sessions = TutoringSession.where("scheduled_datetime > :now", now: Time.now::to_datetime)
-                               .order(:scheduled_datetime)
-  end
+    def create
+        @user = User.new(user_params)
 
-  def schedule_session_student
-    user = User.find(params[:id])
-    tutor_session = TutoringSession.find(params[:sessionID])
+        if @user.save
+            redirect_to @user
+        else
+            render :new
+        end
+    end
 
-    tutor_session.users << user
+    def edit
+        @user = User.find(params[:id])
+    end
 
-    redirect_to '/users/' + params[:id]
-  end
+    def update
+        @user = User.find(params[:id])
 
-  private
-  def user_params
-    params.require(:user).permit(:first_name, :last_name, :major, :email, :encrypted_password)
-  end
+        if @user.update(user_params)
+            redirect_to @user
+        else
+            render :edit
+        end
+    end
+
+    def destroy
+        @user = User.find(params[:id])
+        @user.destroy
+        redirect_to root_path
+    end
+
+    def schedule_student
+        @user = User.find(params[:id])
+        @sessions = TutoringSession.where("scheduled_datetime > :now", now: Time.now::to_datetime)
+                                   .order(:scheduled_datetime)
+    end
+
+    def schedule_session_student
+        user = User.find(params[:id])
+        tutor_session = TutoringSession.find(params[:sessionID])
+
+        tutor_session.users << user
+
+        redirect_to '/users/' + params[:id]
+    end
+
+    private
+    def user_params
+        params.require(:user).permit(:first_name, :last_name, :major, :email, :encrypted_password)
+    end
 end
 
 # t.string "reset_password_token"
