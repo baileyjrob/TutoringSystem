c61f19af4b101323f191a7babce1cf05f33261a2
FrozenLion <28263454+FrozenLion@users.noreply.github.com>
2021-03-22 00:14:29 -0500

    removed student controller and dependencies

--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,11 @@
 
 Rails.application.routes.draw do
   devise_for :users
-  root 'student#index'
-  get 'student/index'
-  get 'student/schedule'
-  post 'student/schedule' => 'student#schedule_session'
+  root 'users#index'
+  get '/users/index', :to => 'users#index'
   get '/users/:id', :to => 'users#show', :as => :user
   get '/users/:id/schedule_student', :to => 'users#schedule_student'
   post '/users/:id/schedule_session_student' => 'users#schedule_session_student'
-  #resources :student, only: [:index, :create, :destroy]
-  #resources :student
   resources :tutoring_session
   resources :user
 end
