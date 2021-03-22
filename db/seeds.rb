9093594289e8deeb174aedb4912f020b2d6e834e
Amanda Latham <alatham@tamu.edu>
2021-03-22 16:07:34 -0500

    navbar made pretty and admin account updated in db seed
    also has role methods added. User's roles displayed on their profile

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
-User.create!(id: 0, first_name: 'Admin', last_name: 'User', email: "admin@tamu.edu", password: "T3st!!a")
 Role.create([{ role_name: 'Admin' }, { role_name: 'Tutor' }, { role_name: 'Student' }, { role_name: 'Spartan Tutor' }])
+User.create!(id: 0, first_name: 'Admin', last_name: 'User', email: "admin@tamu.edu", password: "T3st!!a")
+RoleUser.create(role_id: 1, user_id: 0)
 User.create!(first_name: "John", last_name: "Doe", email: "john@tamu.edu", password: "T3st!!b")
 user2 = User.create!(first_name: "Jane", last_name: "Doe", email: "jane@tamu.edu", password: "T3st!!c")
 user3 = User.create!(first_name: "Jeff", last_name: "Doe", email: "jeff@tamu.edu", password: "T3st!!d")
