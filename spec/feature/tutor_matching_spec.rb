09669bd59e1ac32868c7d98567577b80d6c0365c
Savannah <savyu@tamu.edu>
2021-03-22 21:03:25 -0500

    fixing rubocop errors, continued(3)

--- spec/feature/tutor_matching_spec.rb
+++ spec/feature/tutor_matching_spec.rb
@@ -5,31 +5,31 @@ RSpec.describe 'Tutor Matching', type: :feature do
   describe 'tutor matching' do
     it 'matches tutors using tutor majors' do
       # Create some data
-      user5 = User.create!(id: 15,
+      user1 = User.create!(id: 15,
                            first_name: 'Adam',
                            last_name: 'Doe',
                            major: 'CHEM',
                            email: 'adam@tamu.edu',
                            password: 'T3st!!e')
-      user6 = User.create!(id: 16,
+      user2 = User.create!(id: 16,
                            first_name: 'Ben',
                            last_name: 'Doe',
                            major: 'CHEM',
                            email: 'ben@tamu.edu',
                            password: 'T3st!!f')
-      user7 = User.create!(id: 17,
+      user3 = User.create!(id: 17,
                            first_name: 'Christine',
                            last_name: 'Doe',
                            major: 'CHEM',
                            email: 'christine@tamu.edu',
                            password: 'T3st!!g')
-      user8 = User.create!(id: 18,
+      user4 = User.create!(id: 18,
                            first_name: 'Dakota',
                            last_name: 'Doe',
                            major: 'MATH',
                            email: 'dakota@tamu.edu',
                            password: 'T3st!!h')
-      user9 = User.create!(id: 19,
+      user5 = User.create!(id: 19,
                            first_name: 'Ethan',
                            last_name: 'Doe',
                            major: 'MATH',
