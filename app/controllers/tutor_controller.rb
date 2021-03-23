7e8d463990bd1ab94178e60b68da94f3dae11184
Savannah <savyu@tamu.edu>
2021-03-22 20:44:52 -0500

    fixing rubocop errors, continued

--- app/controllers/tutor_controller.rb
+++ app/controllers/tutor_controller.rb
@@ -1,22 +1,16 @@
 class TutorController < ApplicationController
   def index
-    @test = 'testing'
-    @hello_message = 'hello!'
     @tutors = User.all
-    @matchingTutors = @tutors.where(major: params[:filter_major])
-    @noTutors = '<b> No Available Tutors </b>'.html_safe
-    @availTutors = '<b> Available Tutors </b>'.html_safe
-    @askRequest = ' Would you like to submit a request for a course? '.html_safe
+    @matching_Tutors = @tutors.where(major: params[:filter_major])
+    @no_Tutors = '<b> No Available Tutors </b>'.html_safe
+    @avail_Tutors = '<b> Available Tutors </b>'.html_safe
+    @ask_Request = ' Would you like to submit a request for a course? '.html_safe
 
-    @submittedRequest = :requested_course
+    @submitted_Request = :requested_course
     # user = current_user
     # user.course_name << request
-    @requestSuccessful = 'Request Recieved '.html_safe
+    @request_Successful = 'Request Recieved '.html_safe
 
     # TODO: use roles to filter tutors out
-  end
-
-  def fillRequests
-    redirect_to 'tutor/index'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,20 +3,19 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'registrations' }
   root 'student#index'
-  get 'student/index' => 'tutor#student_index'
   get 'student/schedule'
   post 'student/schedule' => 'student#schedule_session'
   get '/users/:id', :to => 'users#show', :as => :user
-  get 'tutor/index'
-  post 'tutor/index' => 'student#matching_index'
   get 'tutor/request_submission'
-  get '/users/tutor_matching', :to=>'users#tutor_matching'
-  get '/users/course_req_submission', :to=> 'users#course_req_submission'
+  get 'tutor/index', :to => 'tutor#index'
+  #get '/users/tutor_matching', :to=>'users#tutor_matching'
+  #get '/users/course_req_submission', :to=> 'users#course_req_submission'
   get '/users/:id/schedule_student', :to => 'users#schedule_student'
   post '/users/:id/schedule_session_student' => 'users#schedule_session_student'
   #resources :student, only: [:index, :create, :destroy]
   #resources :student
   resources :tutoring_session
-  resources :user
+  resources :users
+  resources :tutors
 
 end
