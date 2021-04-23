a70bbb4902ecb965e5ee6a94fe818944515bc84b
savyu <33470153+savyu@users.noreply.github.com>
2021-04-23 16:21:59 -0500

    cleaned up comments in code

--- app/controllers/course_request_controller.rb
+++ app/controllers/course_request_controller.rb
@@ -4,53 +4,11 @@ require 'user_controller_helper'
 class CourseRequestController < ApplicationController
   protect_from_forgery with: :null_session
 
-  #   def index
-  #     @tutors = User.all #include?(Role.tutor_role)
-  #     @matching_tutors = @tutors.where(major: params[:filter_major])
-  #     @no_tutors = '<b> No Available Tutors </b>'.html_safe
-  #     @avail_tutors = '<b> Available Tutors </b>'.html_safe
-  #     @course_requests = CourseRequest.all
-  #     #redirect_to '/course_request/index'
-  #   end
-  #
-  #
-  #
-  #   def new
-  #     @crequest = CourseRequest.new
-  #   end
-  #
-  #
-  #   def create
-  #     @course_requests = CourseRequest.all
-  #     @ask_request = ' Would you like to submit a request for a course? '.html_safe
-  #     @request_successful = 'Request Recieved '.html_safe
-  #     #@crequest = CourseRequest.new
-  #     @crequest = CourseRequest.new(params[:course_request_params])
-  #     #@crequest.course_name_full = params[:requested_course]
-  #     #@crequest = CourseRequest.new(course_name_full: "...")
-  #     if @crequest.save
-  #       #@submitted_request = params[:requested_course]
-  #       #@crequest.user_id = current_user.id
-  #       redirect_to @crequest, notice: 'Request successfully saved.'
-  #
-  #     else
-  #       render 'new'
-  #     end
-  #   end
-  #
-  #   def show
-  #     #@all_requests = CourseRequest.all
-  #     @crequest = CourseRequest.find(params[:id])
-  #   end
-  #   private
-  #     def course_request_params
-  #       params.require(:course_request).permit(:course_name_full)
-  #     end
   def index
     @course_requests = CourseRequest.all
     @tutor_count = -1
     @tutors = User.all
-    @matching_tutors = @tutors.where(major: params[:filter_major]) # only accepts uppercase
+    @matching_tutors = @tutors.where(major: params[:filter_major]) 
     @tutor_count = 0 if params.key?(:filter_major)
     @matching_tutors.each do |user|
       @tutor_count = @tutor_count.to_i + 1 if user.tutor?
