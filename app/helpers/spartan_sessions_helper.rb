54c5f7928976ef68a78ecf96b0a5434c4777d220
FrozenLion <28263454+FrozenLion@users.noreply.github.com>
2021-04-24 13:24:49 -0500

    added course to spartan session

--- app/helpers/spartan_sessions_helper.rb
+++ app/helpers/spartan_sessions_helper.rb
@@ -4,7 +4,8 @@ module SpartanSessionsHelper
   module_function
 
   def create(params)
-    SpartanSession.create(semester: params[:semester],
+    SpartanSession.create(course: params[:course],
+                          semester: params[:semester],
                           session_datetime: params[:session_datetime],
                           first_code: params[:check_in_code],
                           second_code: params[:check_out_code])
@@ -12,7 +13,8 @@ module SpartanSessionsHelper
 
   def update(params)
     SpartanSession.find(params[:id])
-                  .update(semester: params[:spartan_session][:semester],
+                  .update(course: params[:spartan_session][:course],
+                          semester: params[:spartan_session][:semester],
                           session_datetime: params[:spartan_session][:session_datetime],
                           first_code: params[:spartan_session][:check_in_code],
                           second_code: params[:spartan_session][:check_out_code])
