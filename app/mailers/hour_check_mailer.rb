0ee38039f0cd651cfca32fe1d6455e337bccd4b0
Bailey Robinson <baileyjrob@tamu.edu>
2021-04-28 15:31:57 -0500

    Removed default case for HourMailer to minimize chance for error

--- app/mailers/hour_check_mailer.rb
+++ app/mailers/hour_check_mailer.rb
@@ -3,8 +3,8 @@
 class HourCheckMailer < ApplicationMailer
   default from: 'noreply@tutoringscheduler.com'
 
-  def hours_email(begin_time, end_time, filepath, email = nil)
-    recipient = email.presence || current_user.email
+  def hours_email(begin_time, end_time, filepath, email)
+    recipient = email
     attachments["Tutoring_Hours_#{begin_time}_to_#{end_time}.csv"] = File.read(filepath)
     mail(to: recipient, subject: "Tutoring Hours from #{begin_time} to #{end_time}")
   end
