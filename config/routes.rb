daee0e4281b62a6408a2a60487d4cecf92759a95
Amanda Latham <alatham@tamu.edu>
2021-05-02 16:57:22 -0500

    remove join tutoring session page

--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,7 @@ Rails.application.routes.draw do
   post '/users/admin_view_hours', :to =>'users#output_admin_view_hours'
   get '/users/:id', :to => 'users#show', :as => :user
   post '/users/:id/delete_session', :to => 'users#delete_session', :as => :delete_session
-  get '/users/:id/schedule_student', :to => 'users#schedule_student'
+  #get '/users/:id/schedule_student', :to => 'users#schedule_student'
   post '/users/:id/schedule_session_student' => 'users#schedule_session_student'
 
   post '/course_request/delete_all_request', :to => 'course_request#delete_all_request', :as => :delete_all_request
